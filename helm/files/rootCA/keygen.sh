5d9eeab991f1d79cdbcb1204faf716d63482f697
r-RealG <justmindgame1@gmail.com>
2024-10-18 09:53:29 +0700

    keycloak tls, keycloak StartTls ldap federation set up

--- helm/files/rootCA/keygen.sh
+++ helm/files/rootCA/keygen.sh
@@ -6,6 +6,7 @@ openssl req -x509 -new -nodes -key crts/dolphin.rootCA.key -sha256 -days 3650 -o
 
 
 # Server. Just for reference, not used in the project. Cert-manager will generate the server certificate.
-openssl genpkey -algorithm RSA -out my-org-new.key -pkeyopt rsa_keygen_bits:2048
-openssl req -new -key my-org-new.key -out my-org-new.csr -config dolphin.cnf -reqexts v3_req
-openssl x509 -req -in my-org-new.csr -CA my-org.com.crt -CAkey my-org.com.key -CAcreateserial -out my-org-new.crt -days 365 -extensions v3_req -extfile dolphin.cnf
+# openssl genpkey -algorithm RSA -out my-org-new.key -pkeyopt rsa_keygen_bits:2048
+# openssl req -new -key my-org-new.key -out my-org-new.csr -config dolphin.cnf -reqexts v3_req
+# openssl x509 -req -in my-org-new.csr -CA my-org.com.crt -CAkey my-org.com.key -CAcreateserial \ 
+# -out my-org-new.crt -days 365 -extensions v3_req -extfile dolphin.cnf
