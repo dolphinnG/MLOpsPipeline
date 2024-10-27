9c60942911a522a88410da538ee0a60303a8d522
r-RealG <justmindgame1@gmail.com>
2024-10-27 08:20:50 +0700

    seldon initial setup

---
+++ helm/seldon-install.sh
@@ -0,0 +1,3 @@
+
+
+helm install seldon-core-v2-crds  seldon-charts/seldon-core-v2-crds
--- pytest/kafka-test.sh
+++ pytest/kafka-test.sh
@@ -5,9 +5,11 @@ ssl.truststore.location=/test/ssl/truststore.jks
 ssl.truststore.password=lmaohehe
 sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required \
   username="user1" \
-  password="password1";
+  password="BpS61nZdTX";
 EOF
-
+cat <<EOF > /tmp/client.properties
+security.protocol=PLAINTEXT
+EOF
 
 kubectl run kafka-client -n dolphin-ns --rm -it --image=confluentinc/cp-kafka -- bash 
 
@@ -17,6 +19,8 @@ kafka-console-producer.sh --broker-list kafka-dolphin-service:9092 --topic test_
 kafka-console-consumer.sh --bootstrap-server kafka-dolphin-service:9092 --topic dolphin-topic --from-beginning --consumer.config /tmp/client.properties
 
 kafka-topics.sh --create --topic dolphin-topic --bootstrap-server kafka-dolphin-service:9092 --partitions 1 --replication-factor 1 --command-config /tmp/client.properties
+
+kafka-topics.sh --list --bootstrap-server kafkatest:9092 --command-config /tmp/client.properties
 
 kafka-console-producer.sh --broker-list kafka-dolphin-service:9092 --topic dolphin-topic --producer.config /tmp/client.properties
 
@@ -35,3 +39,15 @@ sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule require
 kubectl run kafka-dolphin-service-client --restart='Never' --image docker.io/bitnami/kafka:3.8.0-debian-12-r5 --namespace dolphin-ns --command -- sleep infinity
 kubectl cp --namespace dolphin-ns .\testclient.properties kafka-dolphin-service-client:/tmp/client.properties
 kubectl exec --tty -i kafka-dolphin-service-client --namespace dolphin-ns -- bash
+
+
+curl -v http://127.0.0.1:80/v2/models/iris-hehe/infer \
+        -H "Content-Type: application/json" \
+        -d '{"inputs": [{"name": "predict", "shape": [1, 4], "datatype": "FP32", "data": [[1, 2, 3, 4]]}]}'
+
+$response = Invoke-WebRequest -Uri "http://127.0.0.1:80/v2/models/iris-hehe/infer" `
+    -Method Post `
+    -ContentType "application/json" `
+    -Body '{"inputs": [{"name": "predict", "shape": [1, 4], "datatype": "FP32", "data": [[1, 2, 3, 4]]}]}'
+
+$response.Content 
